98aa6999ffdd3c8ea04019cdd52504fbffd035ef
Michael Cohen <mcohen01@gmail.com>
2015-01-24 14:57:06 -0800

    payments api sandbox url configuration

--- config.js
+++ config.js
@@ -4,14 +4,9 @@ module.exports = {
   token:           '',
   tokenSecret:     '',
   realmId:         '',
+  useSandbox:      true,
   debug:           false
   //
-  // Uncomment when using the quickbooks sandbox. For info, see the following url:
+  // Set useSandbox to false when moving to production. For info, see the following url:
   // https://developer.intuit.com/v2/blog/2014/10/24/intuit-developer-now-offers-quickbooks-sandboxes
-  //
-  // ,endpointBaseUrl: 'https://sandbox-quickbooks.api.intuit.com/v3/company/'
-  //
-
-  // Payments API Sandbox usage can be configured by uncomenting the following line
-  //paymentsApiUrl: 'https://sandbox.api.intuit.com/quickbooks/v4/payments'
 }
