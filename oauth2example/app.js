d7888e434f9d56d928a7ad0ea2d0b99180bfd4cd
JimmyWongIntuit <jimmy_wong@intuit.com>
2017-10-30 11:47:39 -0700

    Add comments to app.js uri redirect parameters
    Added comments to the uri redirect parameters in the request for access token and authorization token, to remind users that both paths must match exactly to what is specified in the QuickBooks Online Apps dashboard for Redirect URI's.

--- oauth2example/app.js
+++ oauth2example/app.js
@@ -50,7 +50,7 @@ function generateAntiForgery (session) {
 app.get('/requestToken', function (req, res) {
   var redirecturl = QuickBooks.AUTHORIZATION_URL +
     '?client_id=' + consumerKey +
-    '&redirect_uri=' + encodeURIComponent('http://localhost:' + port + '/callback/') +
+    '&redirect_uri=' + encodeURIComponent('http://localhost:' + port + '/callback/') +  //Make sure this path matches entry in application dashboard
     '&scope=com.intuit.quickbooks.accounting' +
     '&response_type=code' +
     '&state=' + generateAntiForgery(req.session);
@@ -71,7 +71,7 @@ app.get('/callback', function (req, res) {
     form: {
       grant_type: 'authorization_code',
       code: req.query.code,
-      redirect_uri: 'http://localhost:' + port + '/callback/'
+      redirect_uri: 'http://localhost:' + port + '/callback/'  //Make sure this path matches entry in application dashboard
     }
   };
 
